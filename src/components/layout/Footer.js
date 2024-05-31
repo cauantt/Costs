99747cc0e65af07a62b95f9dfe17f3cc87e3fa75
Cauã <cauacerqueiranetto2018@gmail.com>
2024-05-31 19:19:04 -0300

    Structured site

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import styles from "./Footer.modules.css"
+
+
+
+function Footer () {
+
+    return(
+        <div></div>
+
+    )
+
+
+}
+
+export default Footer
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import styles from "./Navbar.modules.css"
+
+
+
+function Navbar () {
+
+    return(
+        <div></div>
+
+    )
+
+
+}
+
+export default Navbar
