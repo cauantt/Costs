abf8aa4469b9985b1475822c0f8571f210442d0c
Cauã <cauacerqueiranetto2018@gmail.com>
2024-06-01 15:49:46 -0300

    homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,19 @@
-function Home () {
+import styles from './Home.module.css'
+import savings from "../../img/savings.svg"
+import Linkbutton from '../layout/Linkbutton'
+
+
+ function Home () {
 
     return(
-        <div>
-            <h1>Home</h1>
-          
-        </div>
+        <section className={styles.home_container}>
+            
+            <h1>Welcome to <span>Costs</span></h1>
+            <p>Start managing your projects right now!</p>
+            <Linkbutton to="/newproject" text="Start your project"/>
+            <img src={savings}/>
 
-
+        </section>
     )
 
 
