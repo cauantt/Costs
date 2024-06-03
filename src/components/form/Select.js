a6dfd9fa72d0f9bb436506cc1a95e948756c0601
Cauã <cauacerqueiranetto2018@gmail.com>
2024-06-03 09:19:57 -0300

    api

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -13,6 +13,11 @@ function Select ({ text, name, options, handleOnChange, value}){
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option value={value}>{text}</option>
+                {options.map((option) => (
+
+                    <option value={option.id} key={option.id}>{option.name}</option>
+
+                ))}
             </select>
             
         </div>
--- src/components/project/Projectform.js
+++ src/components/project/Projectform.js
@@ -2,9 +2,26 @@ import styles from './Projectform.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import Submit from '../form/Submit'
+import { useEffect, useState } from 'react'
 
 
 function Projectform ({btnText}) {
+
+     const [categories, setCategories] = useState([])
+
+     useEffect(() => {fetch("http://localhost:5000/categories",{
+        method: "GET",
+        headers: {
+
+            'Content-Type': 'application/json'
+ }
+
+}).then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+.catch(err => console.log(err))},[])
+
     
     return(
 
@@ -16,7 +33,7 @@ function Projectform ({btnText}) {
                 <Input name="Budget" type="text" text="Budget" placeholder="Insert total budget" />
             </div>
             
-                <Select name="category_id" text="Choose an option"/>
+                <Select name="category_id" text="Choose an option" options={categories}/>
             
                 <Submit text={btnText} />
 
